8527446d984c7298f8ce9c6cac60df8f0c9fe133
Robert Yang <campaignjunkie@gmail.com>
2014-01-29 18:27:39 -0500

    added week 1: "Hello World" stuff

---
+++ studio_week1/Assets/scripts/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	// "public" is a keyword that lets other parts of your code see this variable
+	// "public" also exposes the variable to the Unity inspector
+	public string message = "Bonjour monde";
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ( "Hello World! :)" );
+
+		// intent: display "Hello World" in French in our 3D Text
+		GetComponent<TextMesh>().text = message;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
