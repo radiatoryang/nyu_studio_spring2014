9da8dcc0bfa9a64a4c29c16606e6256a32e14f78
Robert Yang <yang.robert.w@gmail.com>
2014-03-12 13:15:44 -0400

    added week 7B1: title screens, instruction screen, persisting data between different level loads using "static"

---
+++ studio_week7/Assets/scripts/HighScore.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScore : MonoBehaviour {
+
+	// this script will remember a high score across playthroughs / level restarts,
+	// until we quit the game... if you want to remember stuff even when the player
+	// quits the game, then use "PlayerPrefs" (look it up in Unity docs)
+
+	// "static" keyword tells Unity to PERSIST this variable across level changes
+	// because, technically, this now lives inside the codebase and not inside a level!
+	static int highScore = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		// press R to reload level, so we can test whether it remembers the score
+		if (Input.GetKeyDown (KeyCode.R)) {
+			Application.LoadLevel ( 2 );
+		}
+		
+		// our amazing game mechanic
+		if (Input.GetKeyDown (KeyCode.Space) ) {
+			// Time.time gives you ENTIRE TIME the game has been running; Time.timeSinceLevelLoad doesn't.
+			highScore = Mathf.RoundToInt (Time.timeSinceLevelLoad);
+		}
+		
+		// display our current score
+		GetComponent<TextMesh>().text = highScore.ToString ();
+	}
+}
---
+++ studio_week7/Assets/scripts/MenuInput.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuInput : MonoBehaviour {
+
+	public KeyCode buttonToPush = KeyCode.Space; // expose to inspector
+	public string nextLevel = "typeInSceneNameHere"; // expose to inspector
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKeyDown ( buttonToPush ) ) {
+			Application.LoadLevel ( nextLevel );
+		}
+	}
+}
